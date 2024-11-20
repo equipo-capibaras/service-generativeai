345bae88b835488ba38effe0ab6f602d360bb121
Laura Molina <ld.molina11@uniandes.edu.co>
2024-11-19 22:47:03 -0500

    :sparkles: added risk incident

--- models/__init__.py
+++ models/__init__.py
@@ -3,7 +3,8 @@ from .channel import Channel
 from .history_entry import HistoryEntry
 from .incident_update_body import IncidentUpdateBody
 from .plan import Plan
+from .risk import Risk
 from .role import Role
 from .user import User
 
-__all__ = ['Action', 'Channel', 'Role', 'Plan', 'User', 'HistoryEntry', 'IncidentUpdateBody']
+__all__ = ['Action', 'Channel', 'Role', 'Plan', 'User', 'HistoryEntry', 'IncidentUpdateBody', 'Risk']
